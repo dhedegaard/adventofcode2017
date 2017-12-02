8a6b1a343de57f4d440960a04b0a58e74fdd121d
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-12-02 21:34:50 +0100

    Fixed ordering when running all the applications through travis

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+find . \
+  -type d \
+  -name "day*" | sort | while read fname
+do
+    echo "*** $fname ***"
+    cd $fname
+    cargo test -q
+    cargo run -q --release
+    cd ..
+done
