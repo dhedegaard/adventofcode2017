6b2b3a478b0027c68cf144bc440f8959f8eca2b8
Dennis Hedegaard <dennis@dhedegaard.dk>
2017-12-11 15:23:08 +0100

    day10: part2, not quiet there yet

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,38 +1,32 @@
 extern crate time;
 
 use time::now;
+use std::fmt::Write;
 
 fn rev_sublist(input: &mut Vec<i32>, index: usize, len: usize) {
     let input_len = input.len();
     let mut sublist = Vec::with_capacity(len);
     {
-        let len = if len > input_len {
-            // This probably never happens :)
-            len % input_len
-        } else {
-            len
-        };
-
         for i in index..index + len {
-            sublist.push(input[i % input_len]);
+            sublist.push(input[i % input_len] % 256);
         }
     }
     let sublist = sublist.iter().rev().collect::<Vec<_>>();
     for i in index..index + len {
-        input[i % input_len] = sublist[i - index].to_owned();
+        input[i % input_len] = sublist[i - index].to_owned() % 256;
     }
 }
 
-fn hash(input: Vec<i32>, input_lengths: Vec<i32>, runs: usize) -> Vec<i32> {
+fn hash(input: Vec<i32>, input_lengths: Vec<usize>, runs: usize) -> Vec<i32> {
     let mut elems = input.iter().map(|e| *e).collect::<Vec<i32>>();
     let mut cur_pos = 0;
     let mut skip_size = 0;
 
     for _ in 0..runs {
         for length in input_lengths.to_owned() {
-            rev_sublist(&mut elems, cur_pos, length as usize);
+            rev_sublist(&mut elems, cur_pos, length);
 
-            cur_pos = (cur_pos + length as usize + skip_size) % input.len();
+            cur_pos = (cur_pos + length + skip_size) % input.len();
             skip_size += 1;
         }
     }
@@ -47,18 +41,57 @@ fn main() {
     {
         let input = INPUT
             .split(",")
-            .map(|e| e.parse::<i32>().unwrap())
+            .map(|e| e.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
         let before = now();
         let result = calculate_result(&hash((0..256).collect::<Vec<_>>(), input, 1));
         println!("part1: {}\ttook: {}", result, now() - before);
     }
     {
+        // Fetch the input as an ASCII byte vector.
         let input = get_input_part2(INPUT);
-        // TODO: let result = hash((0..256).collect::<Vec<_>>(), input, 64);
-        // Build the dense hash
-        // Convert to hex
+        println!("part2: {}", part2(&input));
     }
+}
+
+fn part2(input: &Vec<u8>) -> String {
+    // Do the hashing rounds.
+    let result = hash(
+        (0..256).collect::<Vec<_>>(),
+        input.iter().map(|e| *e as usize).collect::<Vec<_>>(),
+        64,
+    ).iter()
+        .map(|e| *e as u8)
+        .collect::<Vec<_>>();
+    // Build the dense hash
+    let mut dense_result = Vec::with_capacity(result.len() / 16);
+    for i in 0..result.len() / 16 {
+        let slice = result
+            .iter()
+            .skip(i)
+            .take(16)
+            .map(|e| *e)
+            .collect::<Vec<_>>();
+        dense_result.push(dense_hash(&slice));
+    }
+    // Convert to hex
+    vec_to_hex(&dense_result)
+}
+
+fn dense_hash(sparse_hash: &Vec<u8>) -> u8 {
+    let mut result = 0_u8;
+    for e in sparse_hash {
+        result ^= e;
+    }
+    result
+}
+
+fn vec_to_hex(input: &Vec<u8>) -> String {
+    let mut result = String::with_capacity(input.len() * 2);
+    for b in input {
+        write!(&mut result, "{:02x}", b).unwrap();
+    }
+    result
 }
 
 fn get_input_part2(input: &str) -> Vec<u8> {
@@ -98,7 +131,7 @@ mod tests {
     fn test_result1() {
         let input = INPUT
             .split(",")
-            .map(|e| e.parse::<i32>().unwrap())
+            .map(|e| e.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
         let calculated_hash = hash((0..256).collect::<Vec<_>>(), input, 1);
         assert_eq!(calculate_result(&calculated_hash), 212);
@@ -111,6 +144,25 @@ mod tests {
             get_input_part2(&input),
             vec![49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
         );
+    }
+
+    #[test]
+    fn test_vec_to_hex() {
+        assert_eq!(vec_to_hex(&vec![64, 7, 255]), "4007ff");
+    }
+
+    #[test]
+    fn test_dense_hash() {
+        let input = vec![65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22];
+        assert_eq!(dense_hash(&input), 64);
+    }
+
+    #[test]
+    fn test_examples2() {
+        assert_eq!(part2(&get_input_part2("")), "a2582a3a0e66e6e86e3812dcb672a272");
+        assert_eq!(part2(&get_input_part2("AoC 2017")), "33efeb34ea91902bb2f59c9920caa6cd");
+        assert_eq!(part2(&get_input_part2("1,2,3")), "3efbe78a8d82f29979031a4aa0b16a9d");
+        assert_eq!(part2(&get_input_part2("1,2,4")), "63960835bcdc130f0b66d7ff4f6a5a8e");
     }
 }
 
